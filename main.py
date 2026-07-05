6b78e0ff1ef7aef5a800046b7f65ece711ac02c5
mossabphone00-glitch <mossabphone00@gmail.com>
2026-07-05 14:25:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def run_web():
     app_web.run(host='0.0.0.0', port=port)
 
 # --- الإعدادات ---
-ADMIN_IDS = [8642841625, 6022075170] 
+ADMIN_IDS = [8642841625] 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 FONT_FILE = "arial.ttf"
 
